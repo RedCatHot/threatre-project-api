de5868cdebfa13e9f3d2098be5ce76594ea5ea17
Vlad Dukh <vladdukh15@gmail.com>
2023-08-11 16:35:50 +0300

    Add TicketListSerializer, and add taken seats feature

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -54,7 +54,7 @@ class PerformanceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Performance
-        fields = "__all__"
+        fields = ("id", "show_time", "play", "theatre_hall")
 
 
 class PerformanceListSerializer(serializers.ModelSerializer):
@@ -79,9 +79,23 @@ class PerformanceListSerializer(serializers.ModelSerializer):
         )
 
 
-class PerformanceDetailSerializer(PerformanceSerializer):
-    play = PlayListSerializer(many=False, read_only=True)
-    theatre_hall = TheatreHallSerializer(many=False, read_only=True)
+class PerformanceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Performance
+        fields = ("id", "show_time", "play", "theatre_hall")
+
+
+class PerformanceListSerializer(serializers.ModelSerializer):
+    play_title = serializers.CharField(source="play.title", read_only=True)
+    theatre_hall_name = serializers.CharField(
+        source="theatre_hall.name",
+        read_only=True
+    )
+    theatre_hall_capacity = serializers.IntegerField(
+        source="theatre_hall.capacity",
+        read_only=True
+    )
 
     class Meta:
         model = Performance
@@ -114,3 +128,34 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = "__all__"
+
+
+class TicketListSerializer(TicketSerializer):
+    performance = PerformanceListSerializer(many=False, read_only=True)
+
+
+class TicketTakenSeatsSerializer(TicketSerializer):
+
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
+
+
+class PerformanceDetailSerializer(PerformanceSerializer):
+    play = PlayListSerializer(many=False, read_only=True)
+    theatre_hall = TheatreHallSerializer(many=False, read_only=True)
+    taken_places = TicketTakenSeatsSerializer(
+        source="tickets",
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Performance
+        fields = (
+            "id",
+            "show_time",
+            "play_title",
+            "theatre_hall",
+            "taken_places",
+        )
