c2acae3188e358d2ae50ce189c643972f8bc1051
Vlad Dukh <vladdukh15@gmail.com>
2023-08-11 15:26:44 +0300

    Add path to urls

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,1 +1,27 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import (
+    TheatreHallViewSet,
+    GenreViewSet,
+    ActorViewSet,
+    PlayViewSet,
+    PerformanceViewSet,
+    ReservationViewSet,
+    TicketViewSet,
+)
 
+router = routers.DefaultRouter()
+router.register(r'theatrehalls', TheatreHallViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'actors', ActorViewSet)
+router.register(r'plays', PlayViewSet)
+router.register(r'performances', PerformanceViewSet)
+router.register(r'reservations', ReservationViewSet)
+router.register(r'tickets', TicketViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+app_name = "theatre"
