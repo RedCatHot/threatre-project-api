d8ccfa2fe9ebdfbb12e22b604983d111d302b119
Vlad Dukh <vladdukh15@gmail.com>
2023-08-12 23:03:39 +0300

    Add CreateTokenView and path to login

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,7 @@ from user.views import CreateUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login")
 ]
 
 app_name = "user"
