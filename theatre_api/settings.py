c1c1c682ff31b378c9c6a65b2317ab69d5b81cd0
Vlad Dukh <vladdukh15@gmail.com>
2023-09-10 17:23:06 +0300

    Solution

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -5,7 +5,7 @@ from config import SECRET_KEY
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
